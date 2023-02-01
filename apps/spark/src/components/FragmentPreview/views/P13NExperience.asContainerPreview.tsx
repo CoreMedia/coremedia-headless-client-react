a8568623b35a4892ba6dbc2d13df3c32d2f006c1
Eung-Gun Bertelmann <ebertel@coremedia.com>
2023-02-01 18:08:27 +0100

    reset to baseline by 'eye' now possible.

--- apps/spark/src/components/FragmentPreview/views/P13NExperience.asContainerPreview.tsx
+++ apps/spark/src/components/FragmentPreview/views/P13NExperience.asContainerPreview.tsx
@@ -20,8 +20,12 @@ const P13NExperienceAsContainerPreview: React.FC<IncludeProps<P13NExperience>> =
   const Container = slotByName(params?.containerView as string);
   const { previewP13NExperiences } = usePreviewContextState();
   const previewP13Variants = previewP13NExperiences && previewP13NExperiences.variants;
-  const p13nVariants = self && self.variants;
+  //previewP13Variants can contain 'baseline'
+  let p13nVariants = self && self.variants && self.variants.slice();
   const baseline = self && self.baseline as CmTeasable;
+  if (baseline) {
+    p13nVariants?.push({id: 'baseline', target: baseline});
+  }
   let p13nTargets = baseline && [baseline];
   if (previewP13Variants && p13nVariants) {
     p13nTargets = p13nVariants
