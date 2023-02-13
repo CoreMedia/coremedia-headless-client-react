e653dff73709db99ff99a966c2c97692db15e8af
Eung-Gun Bertelmann <ebertel@coremedia.com>
2023-02-13 13:47:19 +0100

    push variant and segment via the window message from simple javascript code.

--- apps/spark/src/p13n/p13n-callback.js
+++ apps/spark/src/p13n/p13n-callback.js
@@ -1,7 +1,65 @@
+const _cm_p13n = {
+
+  multipleSegments: true,
+  segmentSelected: false,
+
+  pushSegment: function (segmentName) {
+    const p13n = window.cm_p13n;
+    if (this.multipleSegments || !this.segmentSelected) {
+      this.segmentSelected = true;
+      p13n.pushVariant(segmentName);
+    }
+  },
+
+  pushVariant: function (variantId) {
+    window.postMessage({type: "cm_p13n", body: {variantId: variantId}})
+  },
+
+  exchangeVariant: function (variantId, contentId) {
+    this.pushVariant(variantId);
+  },
+
+  completed: function (providerId) {
+    const p13n = window.cm_p13n;
+    if (providerId === "ALL") {
+      p13n.ap = [];
+    } else {
+      const index = p13n.ap.indexOf(providerId);
+      if (index > -1) {
+        p13n.ap.splice(index, 1);
+      }
+    }
+    if (p13n.ap.length > 0) {
+      // wait until all providers have finished
+      return;
+    }
+
+    const fragments = $.find(".cm-experience");
+    fragments.forEach(function (f) {
+      const $fragment = $(f);
+
+      const state = $fragment.data("cm-state");
+      if (state !== "loading") {
+        const children = $fragment.children();
+        if (children.length === 1) {
+          const $child = $(children[0]);
+          undecorateNode($fragment);
+          experienceTagAttributes.forEach(function (name) {
+            $child.attr(name, $fragment.attr(name));
+          });
+          $fragment.replaceWith($child);
+          decorateNode($child);
+          $document.trigger(EVENT_NODE_APPENDED, [$child]);
+        }
+      }
+    });
+  }
+};
+
 cm_p13n = {
   ap: [],
   sc: [],
-  _cm_p13n: undefined,
+  _cm_p13n: _cm_p13n,
   registerProvider: function (providerId) {
     cm_p13n.ap.push(providerId);
   },
--- apps/spark/src/pages/Page.tsx
+++ apps/spark/src/pages/Page.tsx
@@ -7,6 +7,7 @@ import { ApolloClientAlert, PageNotFoundAlert } from "../components/Error/Alert"
 import { initializeGrid } from "../models/Grid/Grid";
 import SeoHeader from "../components/Header/SeoHeader";
 import RootPreviewId from "../components/FragmentPreview/RootPreviewId";
+import {PreviewP13NExperiences, usePreviewContextState} from "../context/PreviewContextProvider";
 
 interface PageProps {
   match: match<RouteProps>;
@@ -25,9 +26,22 @@ const Page: FC<PageProps> = ({ match }) => {
     },
   });
 
+  // catch the function to set the preview experience
+  const { setExp2 } = usePreviewContextState();
+
   if (loading) return <Loading />;
   if (error) return <ApolloClientAlert error={error} />;
   if (!data || !data.content || !data.content.pageByPath) return <PageNotFoundAlert />;
+
+  // the p13 hook which is used by studio "eye".
+  window.addEventListener("message", function (event) {
+    const data = event.data;
+    if (data.type === "cm_p13n") {
+      const variantId = data?.body?.variantId;
+      const exp:PreviewP13NExperiences = {variants: [variantId]};
+      setExp2 && setExp2(exp);
+    }
+  }, false);
 
   return (
     <>
